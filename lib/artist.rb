17d628318347e8dfdbfda97e09fe2e5dff77b340
Ji Yim <gsooyim@gmail.com>
2018-05-02 02:08:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,22 @@
 class Artist 
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.find_or_create_by_name(name)
+    if name exists
+      
+    else
+      name = Artist.new(name)
+    end
+  end
  
 end
