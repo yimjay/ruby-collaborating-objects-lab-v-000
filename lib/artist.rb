1cb7c063af6f4a1be84ca7d39f61be7a926a7568
Ji Yim <gsooyim@gmail.com>
2018-05-02 03:09:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,38 @@
-class Artist 
-  attr_accessor :name, :songs
+class Artist
   @@all = []
-  
+  attr_accessor :name, :songs
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def self.all
-    @@all
+    @@all    
   end
-  
+
   def add_song(song)
     @songs << song
   end
-  
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
   def save
     @@all << self
   end
-  
-  def self.find_or_create_by_name(name)
-    @@all.find {|song| song.name == name} || name = Artist.new(name)
-  end
-  
+
   def print_songs
     songs.each {|song| puts song.name}
   end
- 
 end
