402b7e3433ea1b9a5c1cbafd65786dc9ef7135de
Ji Yim <gsooyim@gmail.com>
2018-05-02 02:59:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
   end
   
   def self.new_by_filename(file)
-    artist, song, genre = file.split(" - ")
-    new_song = Song.new(song)
+    artist, name, genre = file.split(" - ")
+    new_song = Song.new(name)
     new_song.artist = artist
   end
 
